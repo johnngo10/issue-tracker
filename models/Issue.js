5f2a9aab7b057286a0babe2937879f4e29624191
xuanngo10 <xuan.ngo10@gmail.com>
2021-01-13 10:22:42 -0800

    cleaned up schema

--- models/Issue.js
+++ models/Issue.js
@@ -39,45 +39,5 @@ const issueSchema = new Schema({
   },
 });
 
-// const issueSchema = new Schema({
-//   project: {
-//     type: String,
-//   },
-//   issues: [
-//     {
-//       issue_title: {
-//         type: String,
-//         required: true,
-//       },
-//       issue_text: {
-//         type: String,
-//         required: true,
-//       },
-//       created_on: {
-//         type: Date,
-//         default: Date.now,
-//       },
-//       updated_on: {
-//         type: Date,
-//         default: Date.now,
-//       },
-//       created_by: {
-//         type: String,
-//         required: true,
-//       },
-//       assigned_to: {
-//         type: String,
-//       },
-//       open: {
-//         type: Boolean,
-//         default: true,
-//       },
-//       status_text: {
-//         type: String,
-//       },
-//     },
-//   ],
-// });
-
 const Issue = mongoose.model('Issue', issueSchema);
 module.exports = Issue;
